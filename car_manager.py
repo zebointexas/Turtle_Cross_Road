210401840678e9a1b57751dc81d87bd62b4fd2c6
HuanRR <mewe0602@hotmail.com>
2023-12-27 00:11:03 -0600

    Color changed

--- car_manager.py
+++ car_manager.py
@@ -1,7 +1,7 @@
 from turtle import Turtle
 import random
 
-COLORS = ["red", "orange", "yellow", "green", "blue", "purple", "pink", "brown"]
+COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
